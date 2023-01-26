7e4c0385a00cae7097fa926dab26bfdd65fa2405
Iminguet <i.minguet@hotmail.com>
2023-01-26 10:17:19 +0100

    Inicio de la app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          tittle: 'Official Guide',
+          description: 'The official VueJs documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tittle: 'Google',
+          description: 'Goolge is cool to look for.',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
